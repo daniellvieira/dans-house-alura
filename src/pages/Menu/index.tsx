5a758cef2ed28b0e859b59bb6d0dd0ebdcd5f2ff
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2022-06-17 11:56:29 -0300

    feat: add baseurl in tsconfig

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,5 +1,5 @@
 import styles from './Menu.module.scss';
-import { ReactComponent as LogoSvg } from '../../assets/logo.svg'; 
+import { ReactComponent as LogoSvg } from 'assets/logo.svg'; 
 
 export default function Menu() {
   return (
