2361a12f8c6a7ffbdf53c7612178b24a143fc517
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2022-06-17 11:52:00 -0300

    feat: configuring application and add svg component

---
+++ src/pages/Menu/index.tsx
@@ -0,0 +1,12 @@
+import styles from './Menu.module.scss';
+import { ReactComponent as LogoSvg } from '../../assets/logo.svg'; 
+
+export default function Menu() {
+  return (
+    <main>
+      <nav className={styles.menu}>
+        <LogoSvg />
+      </nav>
+    </main>
+  )  
+}
