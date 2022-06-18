c5b91d9af7e412f50535a1e1f98cf21cbbdda544
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2022-06-18 11:21:42 -0300

    feat: use absolute import and bem css pattern

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -8,6 +8,12 @@ export default function Menu() {
         <LogoSvg />
         Casa do Dan
       </nav>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+          A casa do código e da massa.
+        </div>
+      </header>
+ 
     </main>
   )  
 }
