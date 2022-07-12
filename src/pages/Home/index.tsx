c0e73d0aa7b769d877ac2b216ccf5f4ecd74d91f
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2022-07-12 17:27:52 -0300

    feat: finished home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import items from 'data/menu.json';
 import styles from './Home.module.scss';
 import stylesTheme from 'styles/Theme.module.scss';
+import ourHouseImg from 'assets/nossa_casa.png';
 
 export default function Home() {
   let recommendedDishes = [...items];
@@ -23,6 +24,13 @@ export default function Home() {
           </div>
         ))
         }
+      </div>
+      <h3 className={stylesTheme.title}>Nossa casa</h3>
+      <div className={styles.ourHouse}>
+        <img src={ourHouseImg} alt='Casa do Aluroni' />
+        <div className={styles.ourHouse__address}>
+          Rua José Cândido, 141 <br /> <br /> Fortaleza - CE
+        </div>
       </div>
     </section>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import DefaultPage from 'components/DefaultPage';
 import TopBar from 'components/TopBar';
+import About from 'pages/About';
 import Home from 'pages/Home';
 import Menu from 'pages/Menu';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -13,6 +14,7 @@ export default function AppRouter() {
           <Route path="/" element={<DefaultPage />}>
             <Route index element={<Home />} />
             <Route path="menu" element={<Menu />} />
+            <Route path="about" element={<About />} />
           </Route>
         </Routes>
       </Router>
