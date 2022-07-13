491b6caccd90c446df2d897d50e00e45b5764a66
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2022-07-13 17:47:39 -0300

    feat: imporved Dish page

--- src/pages/Dish/index.tsx
+++ src/pages/Dish/index.tsx
@@ -1,11 +1,44 @@
 import styles from './Dish.module.scss';
-import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import classNames from 'classnames';
+import items from 'data/menu.json';
 
 const Dish = () => {
-  const params = useParams();
+  const { state } = useLocation();
+  const { dish } = state as { dish: typeof items[0]};
 
   return (
-    <div>Dish {params.id}</div>
+    <>
+      <button className={styles.back}>
+        {' < Voltar'}
+      </button>
+      <section className={styles.container}>
+        <h1 className={styles.title}>{ dish.title }</h1>
+        <div className={styles.image}>
+          <img src={dish.photo} alt={dish.title} />
+        </div>
+        <div className={styles.content}>
+          <p className={styles.content__description}>{ dish.description }</p>
+          <div className={styles.tags}>
+            <div className={classNames({
+              [styles.tags__kind]: true,
+              [styles[`tags__kind__${dish.category.label.toLowerCase()}`]]: true
+            })}>
+              { dish.category.label }
+            </div>
+            <div className={styles.tags__size}>
+              { dish.size }g
+            </div>
+            <div className={styles.tags__serving}>
+              Serve { dish.serving } pessoa{dish.serving === 1 ? '' : 's'}
+            </div>
+            <div className={styles.tags__value}>
+              R$ { dish.price.toFixed(2) }
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,10 +2,16 @@ import items from 'data/menu.json';
 import styles from './Home.module.scss';
 import stylesTheme from 'styles/Theme.module.scss';
 import ourHouseImg from 'assets/nossa_casa.png';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
   let recommendedDishes = [...items];
   recommendedDishes = recommendedDishes.sort(() => 0.5 - Math.random()).splice(0,3);
+  const navigate = useNavigate();
+
+  function redirectToDetails(dish: typeof items[0]) {
+    navigate(`/dishes/${dish.id}`, { state: { dish }, replace: true });
+  }
 
   return (
     <section>
@@ -18,7 +24,10 @@ export default function Home() {
             <div className={styles.recommend__image}>
               <img src={item.photo} alt={item.title} />
             </div>
-            <button className={styles.recommend__button}>
+            <button
+              className={styles.recommend__button}
+              onClick={() => redirectToDetails(item)}
+            >
               Ver mais
             </button>
           </div>
